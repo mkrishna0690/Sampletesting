466d5a79ff940ebbeced379a8c2ed7f098bb4b6f
mkrishna0690 <mkrishna0690@gmail.com>
2019-05-20 11:23:55 -0400

    Add Changesc

---
+++ TestProject/src/com/app/test/SwappingTwoNumers.java
@@ -0,0 +1,24 @@
+package com.app.test;
+
+public class SwappingTwoNumers {
+
+	public static void swapTwoNumbers(String args[]) {
+		
+		int a = 10;
+		int b = 5;
+		
+		System.out.println("BEFORE");
+		System.out.println("--A--:="+a);
+		System.out.println("--B--:="+b);
+		
+		a = a + b;
+		b = a - b;
+		a = a + b;
+		
+
+		System.out.println("AFTER");
+		System.out.println("--A--:="+a);
+		System.out.println("--B--:="+b);
+		
+	}
+}
